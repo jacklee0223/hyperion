a00eac43f537cc91b769f3c711efbc3391d8cb2f
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2015-08-31 20:08:56 -0700

    initial commit

---
+++ views/js/test.js
@@ -0,0 +1,9 @@
+$( document ).ready(function) {
+  $(".dropdown-button").dropdown();
+};
+
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
