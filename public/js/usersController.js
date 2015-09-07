e6d0eb108b5210b182f8c6bd3ca44065c365ca46
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2015-09-06 21:26:36 -0700

    angular working

--- public/js/usersController.js
+++ public/js/usersController.js
@@ -1,40 +1,46 @@
-angular.module('app')
-.controller('UsersController', usersController);
+(function() {
+  angular.module('app')
+         .controller('UsersController', usersController);
 
-usersController.$inject = ['$http'];
+  usersController.$inject = ['$http'];
 
-function UsersController($http){
-  var self = this;
-  self.all = [];
-  self.addUser = addUser;
-  self.newUser = {};
-  self.getUser = getUser;
-  self.deleteUser = deleteUser;
-}
-
-  getUsers();
-  function getUsers(){
-    $http
-      .get('http://localhost:3000/users')
-      .then(function(response){
-        self.all = response.data.users;
-    });
-  }
-
-  function addUser(){
-    $http
-      .post('http://localhost:3000/users', self.newUser)
-      .then(function(response){
-        getUsers();
-    });
+  function UsersController($http){
+    var self = this;
+    self.all = [];
+    self.addUser = addUser;
     self.newUser = {};
+    self.getUser = getUser;
+    self.deleteUser = deleteUser;
   }
 
-  function deleteUser(user){
-    $http
-      .delete("http://localhost:3000/users/" + user._id)
-      .then(function(response){
-        var index = self.all.indexOf(user);
-        self.all.splice(index, 1);
+    getUsers();
+    function getUsers(){
+      $http
+        .get('http://localhost:3000/users')
+        .then(function(response){
+          self.all = response.data.users;
       });
-  }
+    }
+
+    function addUser(){
+      $http
+        .post('http://localhost:3000/users', self.newUser)
+        .then(function(response){
+          getUsers();
+      });
+      self.newUser = {};
+    }
+
+    function deleteUser(user){
+      $http
+        .delete("http://localhost:3000/users/" + user._id)
+        .then(function(response){
+          var index = self.all.indexOf(user);
+          self.all.splice(index, 1);
+        });
+    }
+
+})();
+
+
+
