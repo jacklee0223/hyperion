293ec4c1349dbe88ac001d449327bc53520f9eeb
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2015-09-10 09:30:27 -0700

    procfile fix

--- public/js/arduinoController.js
+++ public/js/arduinoController.js
@@ -44,26 +44,3 @@
              };
           };
 })();
-
-
-//     var app = angular.module('app')
-//                     .factory('mySocket', function (socketFactory) {
-//                         return socketFactory();
-//                     })
-//                     .controller('ArduController', function ($scope,mySocket) {
-
-//                         $scope.ledOn = function () {
-
-//                             mySocket.emit('led:on');
-//                             console.log('LED ON');
-//                         };
-
-
-//                         $scope.ledOff = function () {
-
-//                             mySocket.emit('led:off');
-//                             console.log('LED OFF');
-//                         };
-// });
-
-
